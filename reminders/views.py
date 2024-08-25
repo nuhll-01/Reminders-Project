b842baecfdbff04304aebb05d41d7658747ffa99
Toasty <erick.cool321@icloud.com>
2024-08-24 23:58:37 -0500

    Managed the 'Login' view POST function + Added View

--- reminders/views.py
+++ reminders/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from reminders.models import User
 
 # Create your views here.
 
 class Login(View):
     def get(self, request):
         return render(request, 'login.html')
+
+    def post(self, request):
+        # Extract the POST data
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        if User.objects.filter(username=username, password=password).exists():
+            return redirect('homepage')
+
+class Main(View):
+    def get(self, request):
+        return render(request, 'mainpage.html')
